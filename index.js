7bc930e871bdd923390fb54991d02339651bf3ae
ARROWS-WALL <shamblo511@gmail.com>
2018-12-18 20:38:29 +0900

    htmlをアップデート＆jsファイルを追加

---
+++ index.js
@@ -0,0 +1,11 @@
+function camera_on(){
+  document.getElementById('title').innerHTML = 'ユーザーをサーチ中・・・';
+}
+
+function camera_off(){
+  document.getElementById('title').innerHTML = 'カメラを停止';
+}
+
+function users_info(){
+  window.location.href= "ここに自己紹介ページを貼る";
+}
