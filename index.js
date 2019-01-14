b5bbc8e3a44dfc77bc790f20d7132986a3265b09
ARROWS-WALL <shamblo511@gmail.com>
2019-01-14 23:49:55 +0900

    いらない部分を削除

--- index.js
+++ index.js
@@ -2,10 +2,6 @@ function camera_on(){
   document.getElementById('title').innerHTML = 'ユーザーをサーチ中・・・';
 }
 
-function camera_off(){
-  document.getElementById('title').innerHTML = 'カメラを停止';
-}
-
 function users_info(){
   window.location.href= "self.html";
 }
